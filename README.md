fdca2e5dd26b9f717efbb06678deb37e57567489
Jeff Loucks <jeff@initialstate.com>
2024-05-11 21:06:49 -0400

    More badge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pretty Good UI
 
-Pretty Good UI is a WIP web component library.
+Pretty Good UI is a WIP web component library. I'm building this for fun, and my goal is for it to be... pretty good!
 
 ## Installation
 
--- src/badge.ts
+++ src/badge.ts
@@ -6,30 +6,33 @@ import { customElement } from 'lit/decorators.js'
 export class Badge extends LitElement {
 
 	static styles = css`
-		span {
+		:host {
 			width: fit-content;
 			display: flex;
 			align-items: center;
 			justify-content: center;
-			border: var(--badge-border, 1px solid #99999);
-			border-radius: var(--badge-border-radius, 8px);
-			background-color: var(--badge-bg-color, #DFDFDF);
-			padding: var(--badge-py, 4px) var(--badge-px, 12px);
+			border: var(--border, 1px solid #999999);
+			border-radius: var(--border-radius, 10px);
+			background-color: var(--background-color, #FFFFFF);
+			padding: var(--py, 4px) var(--px, 8px);
+			margin: var(--my, 0) var(--mx, 0);
+			box-shadow: var(--box-shadow, 0);
 		}
-		p {
-			font-family: var(--secondary-font, 'Helvetica');
-			font-size: 12px;
+		p,
+		p ::slotted(*) {
+			color: var(--color, #333333);
+			font-family: var(--font-family, 'Helvetica');
+			font-size: var(--font-size, 12px);
+			font-weight: var(--font-weight, normal);
 			margin: 0;
 		}
 	`
 
 render() {
 	return html`
-	<span>
 		<p>
-			<slot>Badge</slot>
+			<slot></slot>
 		</p>
-	</span>
 	`
 }
 
