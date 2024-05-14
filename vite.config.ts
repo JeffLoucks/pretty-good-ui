83feecf512aaa5aacd859b8b5c87b349574f87a2
Jeff Loucks <jeff@initialstate.com>
2024-05-14 15:59:08 -0400

    Clean up and add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import { resolve } from 'path';
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  return {
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/main.ts'),
-        name: 'Pretty Good UI',
-        fileName: 'pretty-good-ui',
-        formats: ["es"],
-      },
-      rollupOptions: {
-		external: mode === "production" ? "" : /^lit-element/,
-      },
-    },
-  };
+	return {
+		build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.ts'),
+			name: 'Pretty Good UI',
+			fileName: 'pretty-good-ui',
+			formats: ["es"],
+		},
+		rollupOptions: {
+			external: mode === "production" ? "" : /^lit-element/,
+		},
+		},
+		plugins: [dts({ 
+			rollupTypes: true,
+			insertTypesEntry: true,
+			staticImport: true
+		})]
+	};
 });
