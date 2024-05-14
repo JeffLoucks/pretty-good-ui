374a07607f7d78e1457581a0587a87689f05635e
Jeff Loucks <jeff@initialstate.com>
2024-05-14 13:27:18 -0400

    Tests and shaping up package for publishing

--- tests/badge.spec.ts
+++ tests/badge.spec.ts
@@ -12,8 +12,31 @@ import '../src/badge';
 
 describe('badge.ts', () => {
 	it('render shadowDom with an empty slot', async () => {
-		const badge: Badge = await fixture(html`<pg-badge><pg-badge>`)
+		const badge: Badge = await fixture(html`<pg-badge></pg-badge>`)
 		expect(badge).shadowDom.to.equal(`<p><slot></slot></p>`);
 	});
-	
+
+	it('renders content in slot', async () => {
+		const badge: Badge = await fixture(html`<pg-badge>Badge</pg-badge>`)
+		expect(badge).dom.to.equal(`<pg-badge>Badge</pg-badge>`);
+		expect(badge.textContent).to.equal('Badge')
+	});
+
+	it('Badge meets accessibility requirements', async () => {
+		const badge: Badge = await fixture(html`<pg-badge>1</pg-badge>`)
+		expect(badge).is.accessible();
+	});
+
+	it('Badge defaults to 12px', async () => {
+		const badge: Badge = await fixture(html`<pg-badge><h1>Badge</h1></pg-badge>`)
+		const shadowP = getComputedStyle(badge.shadowRoot?.firstElementChild as HTMLParagraphElement)
+		expect(shadowP.fontSize).to.equal("12px")
+	});
+
+	it('Badge defaults to Helvetica', async () => {
+		const badge: Badge = await fixture(html`<pg-badge><h1>Badge</h1></pg-badge>`)
+		const shadowP = getComputedStyle(badge.shadowRoot?.firstElementChild as HTMLParagraphElement)
+		expect(shadowP.fontFamily).to.equal("Helvetica")
+	});
+
 });
