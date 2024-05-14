b7e369c85646fce3c1de9dcb0f76a591f345e26d
Jeff Loucks <jeff@initialstate.com>
2024-05-14 15:46:09 -0400

    multiple entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { resolve } from 'path';
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/main.ts'),
-        name: 'Pretty Good UI',
-        fileName: 'pretty-good-ui',
-        formats: ["es"],
-      },
-      rollupOptions: {
+	build: {
+	lib: {
+		entry: {
+			badge: resolve(__dirname, 'src/badge.ts')
+		},
+		formats: ["es", "cjs"],
+	},
+	rollupOptions: {
 		external: mode === "production" ? "" : /^lit-element/,
-      },
-    },
-  };
+	},
+	},
+	plugins: [dts()]
+};
 });
